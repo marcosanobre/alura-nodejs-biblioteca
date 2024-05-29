fff0cc4e74b859801f3758c4446c9503b3c88de3
Marcos Nobre <marconobre@gmail.com>
2024-05-29 16:36:57 -0300

    atz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/*
+Objetivo deste projeto :
+    Identificar palavras repetidas
+Metas:
+    . obter o texto de um arquivo
+    . criar um array com as palavras do texto
+    . contar ocorrencias
+    . montar um objeto com o resultado
+*/
+
 // carregando a lib de FS
 const fs = require('fs');
 
@@ -11,8 +21,26 @@ const link = caminhoArquivo[2];
 //console.log( link );
 
 fs.readFile( link, 'utf-8', (erro, texto) => {
-    console.log( texto);
+    quebraEmParagrafos( texto );
 });
+
+function quebraEmParagrafos( texto ) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.map( (paragrafo) => {
+        return verificaPalavrasDuplicadas( paragrafo );
+    });
+    console.log( contagem );
+    //console.log( paragrafos );
+};
+
+function verificaPalavrasDuplicadas( texto ) {
+    const listaPalavras = texto.split( ' ' ); // Separador
+    const resultado = {};
+    listaPalavras.forEach( palavra  => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1;
+    });
+    return resultado;
+};
 
 
 
